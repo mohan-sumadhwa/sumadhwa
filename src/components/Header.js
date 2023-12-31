ada2eb90eb14101074b806c77466050294090081
Mohan <mohankv@sumadhwaconsultants.com>
2023-12-31 15:55:50 +0530

    removed basename sumadhwa

--- src/components/Header.js
+++ src/components/Header.js
@@ -73,7 +73,7 @@ function Header({ active }) {
 					{/* <!-- Uncomment below if you prefer to use an image logo --> */}
 					<div className="row justify-content-start">
 						<div className='col-2'>
-							<a href="/sumadhwa">
+							<a href="">
 								<img src={logo} alt="" className="img-fluid" />
 
 								{/* <!-- <h1><a href="index.html">Sumadhwa</a></h1> --> */}
@@ -89,25 +89,25 @@ function Header({ active }) {
 
 				<nav id="navbar" className={`navbar order-last order-lg-0 ${showNavbar && 'navbar-mobile'}`}>
 					<ul>
-						<li><a className={"nav-link scrollto " + (active === "home" ? "active" : "")} href="/sumadhwa">Home</a></li>
+						<li><a className={"nav-link scrollto " + (active === "home" ? "active" : "")} href="/">Home</a></li>
 						{/* <!-- <li><a className="nav-link scrollto" href="#about">About Us</a></li> --> */}
-						<li><a className={"nav-link scrollto " + (active === "about" ? "active" : "")} href="/sumadhwa/about">About</a></li>
+						<li><a className={"nav-link scrollto " + (active === "about" ? "active" : "")} href="/about">About</a></li>
 						{/* <li className="dropdown">
             <a href="#" className={"nav-link scrollto " + ((active == "about" || active == "team") ? "active": "")}><span>About</span> <i className="bi bi-chevron-down"></i></a>
             <ul>
               <li>
-                <a className={"nav-link scrollto " + (active === "about"? "active": "")} href="/sumadhwa/about">Why Sumadhwa</a>
+                <a className={"nav-link scrollto " + (active === "about"? "active": "")} href="/about">Why Sumadhwa</a>
               </li>
               <li>
-                <a className={"nav-link scrollto " + (active === "team"? "active": "")} href="/sumadhwa/team">Core Team</a>
+                <a className={"nav-link scrollto " + (active === "team"? "active": "")} href="/team">Core Team</a>
               </li>
             </ul>
           </li> */}
 						<li>
-							<a className={"nav-link scrollto " + (active === "solutions" ? "active" : "")} href="/sumadhwa/solutions">Solutions</a>
+							<a className={"nav-link scrollto " + (active === "solutions" ? "active" : "")} href="/solutions">Solutions</a>
 						</li>
 						<li>
-							<a className={"contact-link nav-link scrollto " + (active === "contact" ? "active" : "")} href="/sumadhwa/contact">Contact Us</a>
+							<a className={"contact-link nav-link scrollto " + (active === "contact" ? "active" : "")} href="/contact">Contact Us</a>
 						</li>
 					</ul>
 					<i className={`bi ${showNavbar ? "bi-x": "bi-list"} mobile-nav-toggle`} onClick={handleShowNavbar}></i>
